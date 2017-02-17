37e2d193c523c3bc6a2f2159cd39e1177aca5874
LlwydEminence <dywedodd@gmail.com>
2017-02-17 16:42:48 +0700

    Задача Range с первого курса (исправленная)

---
+++ Cft/src/ru/academits/streltsov/cft/Cft.java
@@ -0,0 +1,5 @@
+package ru.academits.streltsov.cft;
+
+
+public class Cft {
+}
--- Range/src/ru/academits/streltsov/range/main/Main.java
+++ Range/src/ru/academits/streltsov/range/main/Main.java
@@ -1,23 +1,23 @@
-package ru.academits.streltsov.range1;
+package ru.academits.streltsov.range.main;
 
+import ru.academits.streltsov.range.Range;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(1, 10);
+        Range range = new Range(1, 12);
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
 
         System.out.print("Введите число: ");
         double point = scanner.nextDouble();
         System.out.println();
-        System.out.println("Длина интервала: " + range.calculateIntervalLength());
+        System.out.println("Длина интервала: " + range.calculateLength());
         if (range.isInside(point)) {
             System.out.print("Число принадлежит интервалу.");
         } else {
             System.out.print("Число не принадлежит интервалу.");
         }
-
     }
 }
